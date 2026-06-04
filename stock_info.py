2758b7f4ddeb3d4ba6640a9ad0318e13c86e9dab
Connor Arnold <con.arnold@outlook.com>
2026-06-04 14:05:20 +0100

    polish: format percentages, add .get() fallbacks for missing data

--- stock_info.py
+++ stock_info.py
@@ -5,22 +5,46 @@ while True:
          break
     try:
         stock = yf.Ticker(ticker)
-        DividendYield = stock.info['dividendYield']
-        PricetoBook = stock.info['priceToBook']
-        averageAnalystRating = stock.info['averageAnalystRating']
-        fiftyDayAverageChangePercent = stock.info['fiftyDayAverageChangePercent']
-        forwardPE = stock.info['forwardPE']
-        marketCap = stock.info['marketCap']
-        forwardEPS = stock.info['forwardEps']
-        sector = stock.info['sector']
-        industry = stock.info['industry']
-        beta = stock.info['beta']
-        print(f"Dividend Yield: {DividendYield}")
+        DividendYield = stock.info.get('dividendYield', '0.00')
+        PricetoBook = stock.info.get('priceToBook', 'N/A')
+        averageAnalystRating = stock.info.get('averageAnalystRating', 'N/A')
+        fiftyDayAverageChangePercent = stock.info.get('fiftyDayAverageChangePercent', 0)
+        forwardPE = stock.info.get('forwardPE', 'N/A')
+        marketCap = stock.info.get('marketCap', 0)
+        forwardEPS = stock.info.get('forwardEps', 'N/A')
+        sector = stock.info.get('sector', 'N/A')
+        industry = stock.info.get('industry', 'N/A')
+        beta = stock.info.get('beta', 'N/A')
+
+        if PricetoBook != 'N/A':
+            PricetoBook = round(PricetoBook, 2)
+
+
+        if marketCap >= 1e12:
+            marketCap_str = f"{round(marketCap / 1e12, 1)}T"
+        elif marketCap >= 1e9:
+            marketCap_str = f"{round(marketCap / 1e9, 1)}B"
+        elif marketCap >= 1e6:
+            marketCap_str = f"{round(marketCap / 1e6, 1)}m"
+        else:
+            marketCap_str = f"{marketCap}"
+
+
+        if forwardPE != 'N/A':
+            forwardPE = round(forwardPE, 2)
+        
+        if forwardEPS != 'N/A':
+            forwardEPS = round(forwardEPS, 2)
+
+        if beta != 'N/A':
+            beta = round(beta, 2)   
+
+        print(f"Dividend Yield: {DividendYield}%")
         print(f"Price to Book: {PricetoBook}")
         print(f"Average Analyst Rating: {averageAnalystRating}")
-        print(f"50 Day Average Change Percent: {fiftyDayAverageChangePercent}")
+        print(f"50 Day Average Change Percent: {round(fiftyDayAverageChangePercent * 100, 1)}%")
         print(f"Forward PE: {forwardPE}")
-        print(f"Market Cap: {round(marketCap,1)}T")
+        print(f"Market Cap: {marketCap_str}")
         print(f"Forward EPS: {forwardEPS}")
         print(f"Sector: {sector}")
         print(f"Industry: {industry}")
@@ -28,4 +52,6 @@ while True:
 
     except KeyError:
         print("This ticker does not exist. Please try again")
+    except ValueError:
+        print("Please enter a valid ticker")
 
